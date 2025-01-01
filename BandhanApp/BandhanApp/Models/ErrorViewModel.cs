3aa2c64484356f70a51749fc2c42702142df59dd
Ketakithakare13 <103560930+Ketakithakare13@users.noreply.github.com>
2025-01-01 23:29:53 +0530

    Add files via upload

---
+++ BandhanApp/BandhanApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BandhanApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
